da109a319e12be83a50fc83a80abdc5ba94a889a
Luiz Castro <lfernando.cwb@gmail.com>
2020-06-05 00:18:19 -0300

    signin page and input/buttom components

---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SigIn from "../../components/signin/signin.component";
+
+import "./signin-and-signup.syles.css";
+import SignIn from "../../components/signin/signin.component";
+
+const SigninAndSignupPage = () => (
+  <div className="signin-and-signup">
+    <SignIn />
+  </div>
+);
+
+export default SigninAndSignupPage;
