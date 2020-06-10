2fc659e162af914ecbe25365dfcfa9b2293eb5ec
Luiz Castro <lfernando.cwb@gmail.com>
2020-06-10 11:42:10 -0300

    stripe payment  integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GsUZYBlcq11oIoumpeTN4cQLhpBzR1fNI0dHs7OnNcMu2jk8B3spjtYtPgYlqywg4JaNosLVnYcnKBPTgttmnF100rrqFRmjh";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Online Store"
+      billingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
